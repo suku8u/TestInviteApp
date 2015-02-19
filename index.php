fd67a27ded1a08cf6bd2147daaa0549c20f1a936
Sukumar Ponram <suku8u@uw.edu>
2015-02-19 12:04:11 +0530

    trying to check php

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-<?php phpinfo(); ?>
+<?php
+// -------------- sqlsrv  LOAD -------------------------------------------
+
+
+ 
+ $server = "tcp:wbqa55dhmb.database.windows.net";
+ $user = "suku@wbqa55dhmb";
+ $pwd = "Stoked88!";
+ $db = "InviteDB";
+
+ try{
+     $conn = new PDO( "sqlsrv:Server= $server ; Database = $db ", $user, $pwd);
+     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+ }
+ catch(Exception $e){
+     die(print_r($e));
+ }
+ 
+
+// -------------------------------------------------------------------------
+
+// -------------- execute query ----------------------------------------------------------------------------------------
+$tsql = "SELECT RSVP FROM dbo.RSVP";
+$stmt = sqlsrv_query( $conn, $tsql);
+
+        if( $stmt === false)
+          {
+             echo "Error in query preparation/execution.\n";
+             die( print_r( sqlsrv_errors(), true));
+          }
+
+?>
