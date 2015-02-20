d028c788b2dddb8a743d1b10b9eb3fd51fc0575a
Sukumar Ponram <suku8u@uw.edu>
2015-02-20 06:13:24 +0530

    trying to check php

--- index.php
+++ index.php
@@ -281,88 +281,88 @@
                            For any question please email: <a href="mailto:youremailhere@emailadd.com">jeanjohnwedding@gmail.com</a>
                         </p>
 
-                        <form id='rsvpForm' method='post' action='?action=add' onsubmit='return false'>
-                        	<div class="sixcol">
-                            	<div class="inputContainer">
-                                    <p>Your Name</p>
-                                    <input type="text" id="name" name="Name" class='validate[required]' />
-                            	</div><!-- .inputContainer -->
-                            	<div class="inputContainer">
-                                	<p>Email</p>
-                                    <input type="text" id="email" name="Email" class='validate[required,custom[email]]' />                                    
-                            	</div><!-- .inputContainer -->
-                                <div class="inputContainer">
-                                    <p>Will you be joining us?</p>
-                                    <input type="radio" name="Answer" value="YES" id="yes" class='validate[required]' /><p class="rd">Yes</p>
-                                    <input type="radio" name="Answer" value="NO" id="no" class='validate[required]' /><p class="rd">No, sorry!</p>
-                                    <p class="rd-msg">Message: <input type="text" id="message" name="Message" placeholder="(optional)" /></p>
-                                </div><!-- .inputContainer -->
-                           	</div><!-- .sixcol -->
-                            
-                            <div class="sixcol last">
-                            	<div class="inputContainer">
-                                    <p># of guest(s) attending</p>
-                                    <input type="text" id="guests" name="Guests-Number" style="width:100px;" />
-                                </div><!-- .inputContainer -->
-                            	<div class="inputContainer">
-                                    <p>Full name of guest(s)</p>
-                                    <textarea id="guest-names" name="Guests-Names"></textarea>
-                                </div><!-- .inputContainer -->
-                                    <button type="submit" id="submit" class="btn">SUBMIT</button>
-                            </div><!-- .sixcol .last -->
-                    	</form>
-						
-						<?php
-						/* Connect to SQL Azure */
-						$server = "tcp:wbqa55dhmb.database.windows.net,1433"; 
-						$user = "suku@wbqa55dhmb";
-						$pass = "Stoked88!";
-						$database = "testinvapp";
+<form id="rsvpForm" method="post" action="?action=add" enctype="multipart/form-data" >
+    Last name <input type="text" name="lastName" id="lastName"/><br />
+    First name <input type="text" name="firstName" id="firstName"/><br />
+    E-mail address <input type="text" name="emailAddress" id="emailAddress"/><br />
+    <input type="submit" name="submit" value="Submit" />
+</form>
  
-						$connectionoptions = array("Database" => $database, 
-						                           "UID" => $user, 
-						                           "PWD" => $pass);
+<?php
+/* Connect to SQL Azure */
+$server = "tcp:wbqa55dhmb.database.windows.net,1433"; 
+$user = "suku@wbqa55dhmb";
+$pass = "Stoked88!";
+$database = "testinvapp";
  
-						$conn = sqlsrv_connect($server, $connectionoptions);
-						if($conn === false)
-						{
-						    die(print_r(sqlsrv_errors(), true));
-						}
-						
-						if(isset($_GET['action']))
-						{
-						    if($_GET['action'] == 'add')
-						    {
-						        /*Insert data.*/
-						        $insertSql = "INSERT INTO RSVP (Name, Email, Answer, Message, Guests-Number, Guests-Names, CreatedDate ) VALUES (?,?,?,?,?,?,?)";
-						        $params = array(&$_POST['Name'], 
-						                        &$_POST['Email'], 
-						                        &$_POST['Answer'], 
-												&$_POST['Messsage'], 
-												&$_POST['Guests-Number'], 
-												&$_POST['Guests-Names'], 
-						                        date("Y-m-d"));
-						        $stmt = sqlsrv_query($conn, $insertSql, $params);
-						        if($stmt === false)
-						        {
-						            /*Handle the case of a duplicte e-mail address.*/
-						            $errors = sqlsrv_errors();
-						            if($errors[0]['code'] == 2601)
-						            {
-						                echo "The e-mail address you entered has already been used.<br />";
-						            }/*Die if other errors occurred.*/
-						            else
-						            {
-						                die(print_r($errors, true));
-						            }
-						        }
-						        else
-						        {
-						            echo "Registration complete.</br>";
-						        }
-						    }
-						}
-						?>
+$connectionoptions = array("Database" => $database, 
+                           "UID" => $user, 
+                           "PWD" => $pass);
+ 
+$conn = sqlsrv_connect($server, $connectionoptions);
+if($conn === false)
+{
+    die(print_r(sqlsrv_errors(), true));
+}
+ 
+if(isset($_GET['action']))
+{
+    if($_GET['action'] == 'add')
+    {
+        /*Insert data.*/
+        $insertSql = "INSERT INTO RegistrationTbl (LastName, FirstName, Email, RegDate) VALUES (?,?,?,?)";
+        $params = array(&$_POST['lastName'], 
+                        &$_POST['firstName'], 
+                        &$_POST['emailAddress'], 
+                        date("Y-m-d"));
+        $stmt = sqlsrv_query($conn, $insertSql, $params);
+        if($stmt === false)
+        {
+            /*Handle the case of a duplicte e-mail address.*/
+            $errors = sqlsrv_errors();
+            if($errors[0]['code'] == 2601)
+            {
+                echo "The e-mail address you entered has already been used.<br />";
+            }/*Die if other errors occurred.*/
+            else
+            {
+                die(print_r($errors, true));
+            }
+        }
+        else
+        {
+            echo "Registration complete.</br>";
+        }
+    }
+}
+ 
+/*Display registered people.*/
+$sql = "SELECT * FROM RegistrationTbl ORDER BY LastName";
+$stmt3 = sqlsrv_query($conn, $sql);
+if($stmt3 === false)
+{
+    die(print_r(sqlsrv_errors(), true));
+}
+ 
+if(sqlsrv_has_rows($stmt3))
+{
+    print("<table border='1px'>");
+    print("<tr><td>Last Name</td>");
+    print("<td>First Name</td>");
+    print("<td>E-mail Address</td>");
+    print("<td>Registration Date</td></tr>");
+    while($row = sqlsrv_fetch_array($stmt3))
+    {
+        $regDate = date_format($row['RegDate'], 'Y-m-d');
+        print("<tr><td>".$row['LastName']."</td>");
+        print("<td>".$row['FirstName']."</td>");
+        print("<td>".$row['Email']."</td>");
+        print("<td>".$regDate."</td></tr>");
+    }
+    print("</table>");
+}
+?>
+
                         
                     	</div><!-- .rsvpParaContainer --> 
                     </div><!-- .twelvecol .rsvp -->
