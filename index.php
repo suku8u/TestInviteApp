689e2b878ed23db4ee2a8568ebf06f88587b7f8d
Sukumar Ponram <suku8u@uw.edu>
2015-02-24 12:31:55 +0530

    landing page pic

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
                         <h1 class="alt" style="margin-top:10px; margin-bottom:20px;"><a href="javascript:void(0)" id="rsvp">rsvp now</a></h1>
                     </div><!-- #invitation -->
                 </div><!-- #invitation-container -->
-                <img src="images/photos/landing-photo.jpg" class="editable" id="landing-photo" width="1050" height="500"/>
+                <img src="images/photos/landing-photo.jpg" class="editable" id="landing-photo" width="1080" height="450"/>
             </div><!-- .landing-row -->
         </div><!-- .landing-container -->
     </section>
