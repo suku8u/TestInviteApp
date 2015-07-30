279a07199e57ee0efb94739c42c8a9a79f0b2362
Sukumar Ponram <suku8u@uw.edu>
2015-07-29 23:34:08 -0400

    Form Changes

--- index.php
+++ index.php
@@ -292,7 +292,7 @@
                 <div class="row">
                     	<div class="rsvpParaContainer">
 
-<iframe src="https://docs.google.com/forms/d/1ClrKfzU6LrBCUMBfFjyABK8Nvd1LM9J4qq0V5skzm44/viewform?embedded=true" width="100%" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe> 
+<iframe src="https://docs.google.com/forms/d/1ClrKfzU6LrBCUMBfFjyABK8Nvd1LM9J4qq0V5skzm44/viewform?embedded=true" width="100%" height="1050" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe> 
                 </div><!-- .row -->
             </div><!-- .rsvpParaContainer .menu5 -->
 		</div><!-- #menu5 --> 
