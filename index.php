d9411ecdcc115c47dbda5ad2fb18da6237be2add
Sukumar Ponram <suku8u@uw.edu>
2015-08-16 11:22:17 -0400

    Update timing

--- index.php
+++ index.php
@@ -201,6 +201,7 @@
                         <h2>Ceremonies</h2>
                         <p>Our American wedding celebration will be held on <br/> 
 							<b>Saturday, Sept 5, 2015</b> at <b>4:00 p.m.</b> <br/>
+							(plan to arrive between 3:00 PM and 3:30 PM to ride gondola to wedding platform)</br>
 							at the Crystal Mountain Resort in Enumclaw, WA.<br/> 
                             <b>33914 Crystal Mountain Blvd. Crystal Mountain, WA 98022</b>
                         </p>
